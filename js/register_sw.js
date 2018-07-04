628e41f809d6a474682fb848d09077e9919edd43
Bruno Mazza <suija87@gmail.com>
2018-07-04 11:10:52 +0200

    Add register sw file to repo

---
+++ js/register_sw.js
@@ -0,0 +1,16 @@
+// Check if service worker is supported in the browser
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('sw/sw.js')
+    .then(registration => {
+        console.log(`Registration successful, scope is ${registration.scope}`);
+    }).catch(error => {
+        console.log(`Service worker registration failed, error: ${error}`);
+    });
+}
+  
+  // Register service worker
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      registration.unregister()
+    }
+  })
