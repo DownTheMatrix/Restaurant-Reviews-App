76521a50eb43954ef6035a5773ae3fef4946b57f
Bruno Mazza <suija87@gmail.com>
2018-07-07 14:00:39 +0200

    Update sw registration function

--- js/register_sw.js
+++ js/register_sw.js
@@ -1,16 +1,16 @@
 // Check if service worker is supported in the browser
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('sw/sw.js')
-    .then(registration => {
-        console.log(`Registration successful, scope is ${registration.scope}`);
-    }).catch(error => {
-        console.log(`Service worker registration failed, error: ${error}`);
-    });
+        .then(registration => {
+            console.log(`Registration successful, scope is ${registration.scope}`);
+        }).catch(error => {
+            console.log(`Service worker registration failed, error: ${error}`);
+        });
 }
-  
-  // Register service worker
-  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+
+// Register service worker
+navigator.serviceWorker.getRegistrations().then(registrations => {
     for (let registration of registrations) {
-      registration.unregister()
+        registration.unregister()
     }
-  })
+})
