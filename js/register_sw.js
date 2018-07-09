0a898df05e45eded55577e7c1db304b2b3473abe
Bruno Mazza <suija87@gmail.com>
2018-07-09 12:27:29 +0200

    Update sw registration code

--- js/register_sw.js
+++ js/register_sw.js
@@ -1,5 +1,34 @@
 'use strict';
-// Check if service worker is supported in the browser
+// Register service worker (src: https://stackoverflow.com/questions/36828088/serviceworkerregistration-active-not-being-set-first-time-chrome)
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(onRegistration);
+}
+
+function onRegistration(registration) {
+  if (registration.waiting) {
+    console.log('waiting', registration.waiting);
+    registration.waiting.addEventListener('statechange', onStateChange('waiting'));
+  }
+
+  if (registration.installing) {
+    console.log('installing', registration.installing);
+    registration.installing.addEventListener('statechange', onStateChange('installing'));
+  }
+
+  if (registration.active) {
+    console.log('active', registration.active);
+    registration.active.addEventListener('statechange', onStateChange('active'));
+  }
+}
+
+function onStateChange(from) {
+  return function(e) {
+    console.log('statechange', from, 'to', e.target.state);
+  }
+}
+
+/* // Check if service worker is supported in the browser
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js')
         .then(registration => {
@@ -14,48 +43,4 @@ navigator.serviceWorker.getRegistrations().then(registrations => {
     for (let registration of registrations) {
         registration.unregister();
     }
-})
-
-/* if ('serviceWorker' in navigator) {
-  // Delay registration until after the page has loaded, to ensure that our
-  // precaching requests don't degrade the first visit experience.
-  // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
-  window.addEventListener('load', function() {
-    // Your service-worker.js *must* be located at the top-level directory relative to your site.
-    // It won't be able to control pages unless it's located at the same level or higher than them.
-    // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
-    // See https://github.com/slightlyoff/ServiceWorker/issues/468
-    navigator.serviceWorker.register('/sw.js').then(function(reg) {
-      // updatefound is fired if service-worker.js changes.
-      reg.onupdatefound = function() {
-        // The updatefound event implies that reg.installing is set; see
-        // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
-        var installingWorker = reg.installing;
-
-        installingWorker.onstatechange = function() {
-          switch (installingWorker.state) {
-            case 'installed':
-              if (navigator.serviceWorker.controller) {
-                // At this point, the old content will have been purged and the fresh content will
-                // have been added to the cache.
-                // It's the perfect time to display a "New content is available; please refresh."
-                // message in the page's interface.
-                console.log('New or updated content is available.');
-              } else {
-                // At this point, everything has been precached.
-                // It's the perfect time to display a "Content is cached for offline use." message.
-                console.log('Content is now available offline!');
-              }
-              break;
-
-            case 'redundant':
-              console.error('The installing service worker became redundant.');
-              break;
-          }
-        };
-      };
-    }).catch(function(e) {
-      console.error('Error during service worker registration:', e);
-    });
-  });
-} */
+}) */
